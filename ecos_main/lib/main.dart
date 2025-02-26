a011fb05f66ede8043790ff739435bea11225260
siddhartha.machi <siddhartha.machi@gmail.com>
2025-02-26 16:39:35 +0530

    Feat custom light theme addition

--- ecos_main/lib/main.dart
+++ ecos_main/lib/main.dart
@@ -1,16 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:json_theme_plus/json_theme_plus.dart';
 
 import 'package:ecos_main/routes/routes.dart';
 
-void main() {
+void main() async {
+  final customTheme = await loadCustomTheme();
+
   runApp(ProviderScope(
     child: MaterialApp.router(
       title: 'Ecos App',
+      theme: customTheme,
       routerDelegate: router.routerDelegate,
       routeInformationParser: router.routeInformationParser,
       routeInformationProvider: router.routeInformationProvider,
     ),
   ));
+}
+
+Future<ThemeData> loadCustomTheme() async {
+  // Fetch custom theme for the user from server
+  // TODO: task defered
+
+  // Need to ensure flutter is initialized before accessing assets.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  var themeStr = await rootBundle.loadString('assets/themes/light_theme.json');
+  var themeJson = jsonDecode(themeStr);
+  final lightTheme = ThemeDecoder.decodeThemeData(themeJson)!;
+
+  // TODO: defered dark theme addition
+  // themeStr = await rootBundle.loadString('assets/themes/dark_theme.json');
+  // themeJson = jsonDecode(themeStr);
+  // final darkThem = ThemeDecoder.decodeThemeData(themeJson)!;
+
+  return lightTheme;
 }
